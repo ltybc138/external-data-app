94649b2b9d71264b7c504de6291ea3e8a1d47197
Semenov Denis <denis.semenov.prog@gmail.com>
2018-08-10 23:51:05 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import IPAddressContainer from './IPAddressContainer';
+
+ReactDOM.render(<IPAddressContainer />, document.getElementById('container'));
